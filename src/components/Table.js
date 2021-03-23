2831fcc2c7690f77b5b086143ecb492595724dbd
victorbunduc <71414528+victorbunduc@users.noreply.github.com>
2021-03-22 22:33:11 -0700

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,79 +10,92 @@ export default class Table extends Component {
     filtered: [],
   };
 
-  // get data from api
+   // API call for randomUser:
   componentDidMount() {
     API.search().then((res) =>
       this.setState({ results: res.data.results, filtered: res.data.results })
     );
   }
-
-  //filter the table data so that it matches search string
-  handleSearchChange = () => {
-    //replenish the search. get the state of employees and the input string
+ // Handle input in search bar:
+    handleSearchChange = () => {
+    
+    
     const searchInput = document.querySelector(".search").value.toLowerCase();
 
-    //filter the array
+    
     const filteredEmployeeArray = this.state.results.filter(
       (employee) => employee.name.first.toLowerCase().indexOf(searchInput) > -1
     );
-    //set the sate to trigger a rerender
+   
     this.setState({
       filtered: filteredEmployeeArray,
     });
   };
-
+// Sort by Name:
   handleNameClick = () => {
-    //get the state of emplyees
-    const employeesArray = this.state.results;
-    //check if accending
+    
+   
+   const employeesArray = this.state.results;
+   
+ 
     if (this.state.order === 1) {
-      //sort the array
+     
       employeesArray.sort((a, b) => {
         return a.name.first.localeCompare(b.name.first);
       });
-      //set the data of the results
-      //set order state to decending
+  
+      
       this.setState({
         results: employeesArray,
         order: -1,
       });
       console.log("ARRAY", employeesArray);
     } else {
-      //sort the array
+ 
       employeesArray.sort((a, b) => {
         return b.name.first.localeCompare(a.name.first);
       });
-      //set the data of the results
-      //set order state to decending
+     
+  
       this.setState({
         results: employeesArray,
         order: 1,
       });
     }
   };
-
+  // Render items:
   render() {
     return (
-      <div>
-       <Search handleSearchChange={this.handleSearchChange}/>
-        <table className="table mt-2">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>
-                Name<button className="btn btn-outline-primary btn-sm ml-1" onClick={this.handleNameClick}>Sort</button>
-              </th>
-              <th>Phone Number</th>
-              <th>Email</th>
-              <th>Date of Birth</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Data employees={this.state.filtered} />
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+     <table className="tableEmployee ">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th onClick={props.sortByName}>Name</th>
+                    <th>Phone</th>
+                    <th>E-mail</th>
+                    <th>DOB</th>
+                </tr>
+            </thead>
+
+            <tbody className= "">
+                {props.results.map(result => (
+                    <tr className="table" key={result.login.uuid}>
+                     
+
+                        <td> <img className="
+                        "src={result.picture.medium} alt="" /></td>
+                        
+                        <td>{result.name.first + " " + result.name.last}  </td>
+
+                        <td>{result.cell}</td>
+                        <td className="email"><a href={result.email}>{result.email}</a></td>
+                        <td><Moment format="MM/DD/YYYY">{result.dob.date}</Moment></td>
+
+                    </tr>
+                ))}
+            </tbody>
+        </table >
+    )
+
 }
+export default Table;
